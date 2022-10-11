17d0294871e0859bc71835aab790362f80d74c4f
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-10-11 20:05:50 +0545

    removed a state

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,18 @@
-import react, { useEffect, useState } from "react";
+import react, { useState } from "react";
 import MapContainer from "../components/MapContainer";
 import newsJson from "../utils/news.json";
 
 export default function Home() {
-  const [news, setNews] = useState([]);
-  const [currentNews, setCurrentNews] = useState([]);
+  const [news, setNews] = useState(newsJson.slice(0, 5));
 
   const loadData = () => {
-    const currentDataLength = currentNews.length;
-    setCurrentNews(news.slice(0, currentDataLength + 5));
+    const currentDataLength = news.length;
+    setNews(newsJson.slice(0, currentDataLength + 5));
   };
-
-  useEffect(() => {
-    setNews(newsJson);
-    setCurrentNews(newsJson.slice(0, 5));
-  }, []);
 
   return (
     <div>
-      <MapContainer news={currentNews} loadData={loadData} />
+      <MapContainer news={news} loadData={loadData} />
     </div>
   );
 }
