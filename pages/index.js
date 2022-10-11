117b21d4bb8d1fddb8cf9ac3e46df355dd19a89a
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-10-11 20:32:03 +0545

    removed news state

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,11 @@ import newsJson from "../utils/news.json";
 
 export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
-  const [news, setNews] = useState([]);
+  const news = newsJson.slice(0, currentPage * 5);
 
   const loadData = () => {
     setCurrentPage(currentPage + 1);
   };
-
-  useEffect(() => {
-    setNews(newsJson.slice(0, currentPage * 5));
-  });
 
   return (
     <div>
