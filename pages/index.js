4241464d8401ea17af8400e486bd70b7524ab382
Ashutosh Bhandari <ashutoshbhandari5@gmail.com>
2022-10-12 14:42:57 +0545

    added useMemo

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from "react";
+import react, { useEffect, useMemo, useState } from "react";
 import MapContainer from "../components/MapContainer";
 import newsJson from "../utils/news.json";
 import filterJson from "../utils/filter.json";
@@ -7,17 +7,21 @@ export default function Home() {
   const [currentPage, setCurrentPage] = useState(1);
   const [filter, setFilter] = useState({ sortBy: "author", order: "asc" });
   const [selectedNews, setSelectedNews] = useState(null);
-  const news = newsJson
-    .sort((a, b) => {
-      return filter.order === "asc"
-        ? a[filter.sortBy].localeCompare(b[filter.sortBy])
-        : b[filter.sortBy].localeCompare(a[filter.sortBy]);
-    })
-    .slice(0, currentPage * 5);
+  const news = useMemo(
+    () =>
+      newsJson
+        .sort((a, b) => {
+          return filter.order === "asc"
+            ? a[filter.sortBy].localeCompare(b[filter.sortBy])
+            : b[filter.sortBy].localeCompare(a[filter.sortBy]);
+        })
+        .slice(0, currentPage * 5),
+    [currentPage, filter]
+  );
 
   useEffect(() => {
     setSelectedNews(null);
-  }, [filter, news.length]);
+  }, [filter, news]);
 
   const loadData = () => {
     setCurrentPage(currentPage + 1);
